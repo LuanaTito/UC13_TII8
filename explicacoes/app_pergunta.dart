08e69f67e0a2e1195ea4abbac9e832cc4eb6d901
LuanaTito <111540897+LuanaTito@users.noreply.github.com>
2025-02-11 20:55:28 -0300

    explicacao4

--- explicacoes/app_pergunta.dart
+++ explicacoes/app_pergunta.dart
@@ -375,3 +375,75 @@ class PerguntaApp extends StatefulWidget {
 //escolher stateless
 //dar o nome e digitar ctrl . para importar o material design
 
+import 'package:flutter/material.dart';
+
+class Questao extends StatelessWidget {
+  final String texto;
+
+  const Questao(this.texto, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(texto);
+  }
+}
+
+    //no arquivo main.dart
+
+    import 'package:flutter/material.dart';
+import './questao.dart';
+
+void main() => runApp(const PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Questao(perguntas[_perguntaSelecionada]), //adicionar o componente aqui.
+            ElevatedButton(
+              onPressed: _responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: _responder,
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: _responder,
+              child: const Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+
+  @override
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
