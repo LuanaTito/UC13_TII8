9e462970e90c221f7a3e3edcf1661d7d0571162e
unknown <luana.dmelo@senacsp.edu.br>
2025-02-11 17:06:23 -0300

    explicacao

--- UC13/lib/main.dart
+++ UC13/lib/main.dart
@@ -1,51 +1,56 @@
-
 import 'package:flutter/material.dart';
 
 main() => runApp(PerguntasApp());
 
-class PerguntasApp extends StatelessWidget{
+class PerguntasApp extends StatelessWidget {
   //metodo criado para a classe e POO
-  void responder(){
+  void responder() {
     print('Pergunta resposndida!');
   }
 
-@override
+  @override
   Widget build(BuildContext context) {
-    final List<String> perguntas =[ //variável que contém um lista
+    final List<String> perguntas = [
+      //variável que contém um lista
       'Qual é a sua cor favorita?',
       'Qual é o seu animal favorito?',
       'qual é a sua comida favorita?',
     ];
 
-      return MaterialApp(//material design
-      home: Scaffold( //estrutura da aplicação
-        appBar: AppBar(//topo
+    return MaterialApp(
+      //material design
+      home: Scaffold(
+        //estrutura da aplicação
+        appBar: AppBar(
+          //topo
           title: Text('Perguntas'),
         ),
         //body: Text('Olá Mundo!') //teste de página
         body: Column(
           children: <Widget>[
-            Text(perguntas.elementAt(0)),
+           // Text(perguntas.elementAt(0)),
             Text(perguntas[1]),
-          ElevatedButton(//recebe dois parametros
-          child: Text('Resposta1'),//crio um botão que recebe outro widget um filho, que pode ser uma coluna, linha, texto, icone...
-          onPressed: responder, //parametros de click
+            ElevatedButton(
+              //recebe dois parametros
+              child: Text(
+                  'Resposta1'), //crio um botão que recebe outro widget um filho, que pode ser uma coluna, linha, texto, icone...
+              onPressed: responder, //parametros de click
             ),
-            ElevatedButton(//recebe dois parametros
-          child: Text('Resposta2'),//crio um botão que recebe outro widget um filho, que pode ser uma coluna, linha, texto, icone...
-          onPressed: responder, //parametros de click
+            ElevatedButton(
+              //recebe dois parametros
+              child: Text(
+                  'Resposta2'), //crio um botão que recebe outro widget um filho, que pode ser uma coluna, linha, texto, icone...
+              onPressed: responder, //parametros de click
             ),
-            ElevatedButton(//recebe dois parametros
-          child: Text('Resposta3'),//crio um botão que recebe outro widget um filho, que pode ser uma coluna, linha, texto, icone...
-          onPressed: responder, //parametros de click
+            ElevatedButton(
+              //recebe dois parametros
+              child: Text(
+                  'Resposta3'), //crio um botão que recebe outro widget um filho, que pode ser uma coluna, linha, texto, icone...
+              onPressed: responder, //parametros de click
             ),
-
-            
           ],
-         
         ),
       ),
-
     );
   }
 }
--- explicacoes/app_pergunta.dart
+++ explicacoes/app_pergunta.dart
@@ -188,5 +188,55 @@ class PerguntasApp estends StatelessWidget {
     );
   }
 }      
-//parei em funcao anonima 
+
+////========***********==========
+///
+///transformando em stateful
       
+
+
+void main() {
+  runApp(const PerguntasApp());
+}
+
+class PerguntasApp extends StatelessWidget {
+  const PerguntasApp({super.key});
+
+  void responder() {
+    print('Pergunta respondida!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[1]),
+            ElevatedButton(
+              onPressed: responder,
+              child: const Text('Resposta 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Resposta 2 foi selecionada!');
+              },
+              child: const Text('Resposta 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('esposta 3 !!'),
+              child: const Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
