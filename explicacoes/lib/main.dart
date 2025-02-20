d2c0da333581169d7c1afcd4fd3d3c8ebdcd8156
LuanaTito <111540897+LuanaTito@users.noreply.github.com>
2025-02-20 07:31:24 -0300

    Update main.dart

--- explicacoes/lib/main.dart
+++ explicacoes/lib/main.dart
@@ -36,3 +36,49 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
+
+//###########___________##########
+//ajustando o scroll
+import 'package:flutter/material.dart';
+import 'components/transaction_user.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      //envolver a Column
+      body: SingleChildScrollView( //quando buscar por padrão não existe o componete, então envolver em widget e depois alterar o nome para Single...
+        //para funcionar preciso colocar uma altura na lista.dart
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const [
+            SizedBox(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            TransactionUser(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+//ir para list
