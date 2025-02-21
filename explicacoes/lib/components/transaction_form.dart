fa3bb9460859259de6ef6f00673646584a178974
LuanaTito <111540897+LuanaTito@users.noreply.github.com>
2025-02-20 22:13:26 -0300

    20_02

--- explicacoes/lib/components/transaction_form.dart
+++ explicacoes/lib/components/transaction_form.dart
@@ -60,4 +60,80 @@ class TransactionForm extends StatelessWidget {
 
 //alterar o user, o parametro do forms
 //TransactionForm(_addTransaction),
+//##########------------
+
+//inserindo o teclado numerico
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  //criar uma função para submeter o formulario
+  final void Function(String, double) onSubmit;
+  //programando o enviar
+  TransactionForm(this.onSubmit, {Key? key}) : super(key: key);
+//função para transformar o alfanumerico em apenas númerico 
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0;
+
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+
+    onSubmit(title, value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              onSubmitted: (_) => _submitForm(), //declaração do teclado alfanumérico
+              decoration: const InputDecoration(
+                labelText: 'Título',
+              ),
+            ),
+            TextField(
+              controller: valueController,
+              //inserindo o teclado numérico
+               keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submitForm(),
+              decoration: const InputDecoration(
+                labelText: 'Valor (R\$)',
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text(
+                    'Nova Transação',
+                    style: TextStyle(
+                      color: Colors.purple,
+                    ),
+                  ),
+                  //programação do botão que irá receber as informações do forms
+                  // onPressed: () {
+                  //   final title = titleController.text;
+                  //   final value = double.tryParse(valueController.text) ?? 0;
+                  //   onSubmit(title, value);
+                  // },
+                   onPressed: _submitForm,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
