55dd125a86500c2020a3a9f3231863fab31d0907
LuanaTito <111540897+LuanaTito@users.noreply.github.com>
2025-02-24 18:09:35 -0300

    Update main.dart

--- explicacoes/lib/main.dart
+++ explicacoes/lib/main.dart
@@ -82,3 +82,112 @@ class MyHomePage extends StatelessWidget {
   }
 }
 //ir para list
+
+
+//###########___________##########
+//Inserindo botões para fazer modal 
+import 'package:flutter/material.dart';
+import 'components/transaction_user.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+        actions: [ // adicionando um widget para ações em cima (appBar - topo)
+          IconButton(
+            icon: const Icon(Icons.add), //ao mostrar a lista de icones, pode clicar no simbolo de informação, ele mostra o icone selecionado
+            onPressed: () {}, // de começo um função vazia.
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const [
+            SizedBox(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            TransactionUser(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton( // é possivel colocar também na parte de baixo (roapé do aprocativo) dentro ainda do scaffold.
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, // localização do botão (centro, começo e final) 
+    );
+  }
+}
+
+//###########___________##########
+//Construindo o  modal 
+import 'package:flutter/material.dart';
+import 'components/transaction_user.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+        actions: [ // adicionando um widget para ações em cima (appBar - topo)
+          IconButton(
+            icon: const Icon(Icons.add), //ao mostrar a lista de icones, pode clicar no simbolo de informação, ele mostra o icone selecionado
+            onPressed: () {}, // de começo um função vazia.
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const [
+            SizedBox(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            TransactionUser(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton( // é possivel colocar também na parte de baixo (roapé do aprocativo) dentro ainda do scaffold.
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat, // localização do botão (centro, começo e final) 
+    );
+  }
+}
